7ee9da40348e06b1de1a28ff7949e11e1df261d1
Sam <decoyoctorok@gmail.com>
2023-07-15 23:38:14 -0700

    Render appointment datetimes

--- src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
@@ -9,6 +9,8 @@ export const AppointmentDetails: React.FC<Props> = ({ appointment }) => {
     return (
         <div id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
             <h3>{appointment.appointmentType}</h3>
+            <div>Request Created: {appointment.createDateTime?.toLocaleString()}</div>
+            <div>Requested Appt Date: {appointment.requestedDateTimeOffset?.toLocaleString()}</div>
             <div className="appointment-detail-body">
                 <div>User: {appointment.user?.givenName} {appointment.user?.familyName}</div>
                 <div>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</div>
