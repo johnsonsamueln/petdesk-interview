07624a9ad6b80cda43bdd99ed8d599591f32717b
johnsonsamueln <38303032+johnsonsamueln@users.noreply.github.com>
2023-07-16 11:22:57 -0700

    Initial structuring & styling of the appointments list (#5)
    ![image](https://github.com/johnsonsamueln/petdesk-interview/assets/38303032/e6bd63b5-3f8c-47a0-9604-716c13e3d6aa)
    
    
    The CRA template came with Bootstrap. To get things started, I made use of some Bootstrap for the layout positioning

---
+++ src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
@@ -0,0 +1,81 @@
+import * as React from "react";
+import { Animal, Appointment, Species, User } from "../../../types/appointments/ui";
+import { ReactComponent as UserIcon } from "../../../content/user.svg";
+import { ReactComponent as DogIcon } from "../../../content/dog.svg";
+import { ReactComponent as CatIcon } from "../../../content/cat.svg";
+import { ReactComponent as BirdIcon } from "../../../content/bird.svg";
+import { ReactComponent as PawIcon } from "../../../content/paw.svg";
+import { ReactComponent as ClockIcon } from "../../../content/clock.svg";
+import "./AppointmentDetail.css"
+
+type AppointmentDetailsProps = {
+    appointment: Appointment
+}
+export const AppointmentDetails: React.FC<AppointmentDetailsProps> = ({ appointment }) => {
+    return (
+        <div id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
+            <h3>{appointment.appointmentType}</h3>
+            <div className="appointment-detail-body">
+                <div className="appointment-members">
+                    <UserDetails user={appointment.user} />
+                    <AnimalDetails animal={appointment.animal} />
+                    <ScheduleDetails appointment={appointment} />
+                </div>
+                <div className="appointment-actions">
+                    <button className="appointment-action-button confirm-action" title="Confirm appointment for the requested time">Confirm</button>
+                    <button className="appointment-action-button reschedule-action" title="Request a different time for this appointment">Reschedule</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+type UserDetailsProps = {
+    user: User | null;
+}
+export const UserDetails: React.FC<UserDetailsProps> = ({ user }) => {
+    return (
+        <div className="appointment-member-detail user-detail">
+            <UserIcon />
+            <span>{user?.givenName} {user?.familyName}</span>
+        </div>
+    )
+}
+
+type AnimalDetailsProps = {
+    animal: Animal | null;
+}
+export const AnimalDetails: React.FC<AnimalDetailsProps> = ({ animal }) => {
+    let icon: React.ReactNode;
+    switch (animal?.species) {
+        case Species.Dog:
+            icon = <DogIcon />
+            break;
+        case Species.Bird:
+            icon = <BirdIcon />
+            break;
+        case Species.Cat:
+            icon = <CatIcon />
+            break;
+        default:
+            icon = <PawIcon />
+    }
+
+    let breed = animal?.breed && `(${animal.breed})`
+
+    return (
+        <div className="appointment-member-detail animal-detail">
+            {icon}
+            <span>{animal?.givenName} {breed}</span>
+        </div>
+    )
+}
+
+export const ScheduleDetails: React.FC<AppointmentDetailsProps> = ({ appointment }) => {
+    return (
+        <div className="appointment-member-detail schedule-detail">
+            <ClockIcon />
+            <span>{appointment.requestedDateTimeOffset?.toLocaleString()}</span>
+        </div>
+    )
+}
--- src/ClientApp/src/components/sections/Appointments/AppointmentsSection.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentsSection.tsx
@@ -3,8 +3,11 @@ import { Appointment, Species } from "../../../types/appointments/ui"
 import { AppointmentResponse } from "../../../types/appointments/api";
 import { getDateOrDefault } from "../../../helpers/date";
 import { API_ROUTES } from "../../../constants/api-routes";
+import { AppointmentDetails } from "./AppointmentDetails";
+import "./AppointmentsSection.css"
 
 export const AppointmentsSection: React.FC = () => {
+    const [isLoading, setIsLoading] = React.useState(true);
     const [appointments, setAppointments] = React.useState<Appointment[]>([]);
 
     React.useEffect(() => {
@@ -12,37 +15,37 @@ export const AppointmentsSection: React.FC = () => {
             const fetchResponse = await fetch(API_ROUTES.APPOINTMENTS_GET, { method: "GET" });
             const appointmentsResponse: AppointmentResponse = await fetchResponse.json();
 
-            const clientAppointments: Appointment[] = appointmentsResponse.appointments.map(serverAppointment => ({
-                ...serverAppointment,
-                createDateTime: getDateOrDefault(serverAppointment.createDateTime),
-                requestedDateTimeOffset: getDateOrDefault(serverAppointment.requestedDateTimeOffset),
-                animal: serverAppointment.animal && {
-                    ...serverAppointment.animal,
-                    species: serverAppointment.animal.species as Species
-                }
-            }))
+            const clientAppointments: Appointment[] = toUIAppointments(appointmentsResponse);
 
-            setAppointments(clientAppointments);
+                setAppointments(clientAppointments);
+            setIsLoading(false);
         }
         initializeAppointments();
     }, [])
 
     return (
-        <div id="appointments-section">
+        <div id="appointments-section" className="container">
             <h1>Appointments</h1>
-            <div id="appointments-list">
-                <ul>
-                    {appointments.map(appointment => (
-                        <li key={appointment.appointmentId} id={`appointment-${appointment.appointmentId}`}>
-                            <ul>
-                                <li>{appointment.appointmentType}</li>
-                                <li>User: {appointment.user?.givenName} {appointment.user?.familyName}</li>
-                                <li>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</li>
-                            </ul>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+            <ul id="appointments-list" className="appointments-list">
+                {isLoading && (<span className="spinner-border" />)}
+                {appointments.map(appointment => (
+                    <li key={appointment.appointmentId} id={`appointment-${appointment.appointmentId}`}>
+                        <AppointmentDetails appointment={appointment} />
+                    </li>
+                ))}
+            </ul>
         </div>
     )
+}
+
+const toUIAppointments = (appointmentsResponse: AppointmentResponse): Appointment[] => {
+    return appointmentsResponse.appointments.map(serverAppointment => ({
+        ...serverAppointment,
+        createDateTime: getDateOrDefault(serverAppointment.createDateTime),
+        requestedDateTimeOffset: getDateOrDefault(serverAppointment.requestedDateTimeOffset),
+        animal: serverAppointment.animal && {
+            ...serverAppointment.animal,
+            species: serverAppointment.animal.species as Species
+        }
+    }))
 }
