0f4d4a241b18a89d0a6260805b4da012121888f7
Sam <decoyoctorok@gmail.com>
2023-07-15 23:19:29 -0700

    Changed detail to not be a list

--- src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
@@ -7,10 +7,10 @@ type Props = {
 }
 export const AppointmentDetails: React.FC<Props> = ({ appointment }) => {
     return (
-        <ul id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
-            <li>{appointment.appointmentType}</li>
-            <li>User: {appointment.user?.givenName} {appointment.user?.familyName}</li>
-            <li>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</li>
-        </ul>
+        <div id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
+            <h3>{appointment.appointmentType}</h3>
+            <div>User: {appointment.user?.givenName} {appointment.user?.familyName}</div>
+            <div>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</div>
+        </div>
     )
 }
