28dd0b922cc2f24e2fa188e8dddae2e687616acc
Sam <decoyoctorok@gmail.com>
2023-07-15 20:13:06 -0700

    Added fields to User and Animal models

--- src/External/Pstmn/Models/User.cs
+++ src/External/Pstmn/Models/User.cs
@@ -2,5 +2,9 @@ namespace petdesk_interview_app.External.Pstmn.Models
 {
     public class User
     {
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? VetDataId { get; set; }
     }
 }
