aecc400e219a6e531ed5178d4c4c6046de99eb31
Sam <decoyoctorok@gmail.com>
2023-07-16 09:42:52 -0700

    Extracted components for User and Animal sections

--- src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
@@ -1,20 +1,38 @@
 import * as React from "react";
-import { Appointment } from "../../../types/appointments/ui";
+import { Animal, Appointment, User } from "../../../types/appointments/ui";
 import "./AppointmentDetail.css"
 
-type Props = {
+type AppointmentDetailsProps = {
     appointment: Appointment
 }
-export const AppointmentDetails: React.FC<Props> = ({ appointment }) => {
+export const AppointmentDetails: React.FC<AppointmentDetailsProps> = ({ appointment }) => {
     return (
         <div id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
             <h3>{appointment.appointmentType}</h3>
             <div>Request Created: {appointment.createDateTime?.toLocaleString()}</div>
             <div>Requested Appt Date: {appointment.requestedDateTimeOffset?.toLocaleString()}</div>
             <div className="appointment-detail-body">
-                <div>User: {appointment.user?.givenName} {appointment.user?.familyName}</div>
-                <div>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</div>
+                <UserDetails user={appointment.user} />
+                <AnimalDetails animal={appointment.animal} />
             </div>
         </div>
+    )
+}
+
+type UserDetailsProps = {
+    user: User | null;
+}
+export const UserDetails: React.FC<UserDetailsProps> = ({ user }) => {
+    return (
+        <div>User: {user?.givenName} {user?.familyName}</div>
+    )
+}
+
+type AnimalDetailsProps = {
+    animal: Animal | null;
+}
+export const AnimalDetails: React.FC<AnimalDetailsProps> = ({ animal }) => {
+    return (
+        <div>Animal: {animal?.givenName} {`(${animal?.species}, ${animal?.breed})`}</div>
     )
 }
