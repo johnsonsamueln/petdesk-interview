ae0bf4b9a6ab2918ced235a61e79a30650fbc74a
Sam <decoyoctorok@gmail.com>
2023-07-15 20:26:49 -0700

    Inject httpClientFactory into service

--- src/External/Pstmn/Pstmn.cs
+++ src/External/Pstmn/Pstmn.cs
@@ -4,6 +4,13 @@ namespace petdesk_interview_app.External.Pstmn;
 
 public class Pstmn : IPstmn
 {
+    private readonly IHttpClientFactory httpClientFactory;
+
+    public Pstmn(IHttpClientFactory httpClientFactory)
+    {
+        this.httpClientFactory = httpClientFactory;
+    }
+
     public Task<Appointment[]> GetAppointments()
     {
         throw new NotImplementedException();
