f6df370fa981217c3191ace923f37a31070e8773
Sam <decoyoctorok@gmail.com>
2023-07-15 20:44:32 -0700

    Copied models for ControllerModels

--- src/Controllers/Appointments/Models/Appointment.cs
+++ src/Controllers/Appointments/Models/Appointment.cs
@@ -2,11 +2,10 @@ namespace petdesk_interview_app.Controllers.Appointments.Models;
 
 public class Appointment
 {
-    public DateTime Date { get; set; }
-
-    public int TemperatureC { get; set; }
-
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-    public string? Summary { get; set; }
+    public int AppointmentId { get; set; }
+    public string? AppointmentType { get; set; }
+    public DateTime? CreateDateTime { get; set; }
+    public DateTimeOffset? RequestedDateTimeOffset { get; set; }
+    public User? User { get; set; }
+    public Animal? Animal { get; set; }
 }
---
+++ src/Controllers/Appointments/Models/User.cs
@@ -0,0 +1,10 @@
+namespace petdesk_interview_app.Controllers.Appointments.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? VetDataId { get; set; }
+    }
+}
