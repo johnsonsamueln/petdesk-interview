7c1a057de3e675db612361ab0b26159c0ba4cc32
Sam <decoyoctorok@gmail.com>
2023-07-15 21:43:26 -0700

    Implemented basic rendering logic of appointment state

--- src/ClientApp/src/components/sections/Appointments/AppointmentsSection.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentsSection.tsx
@@ -1,9 +1,25 @@
 import * as React from "react"
+import { Appointment } from "../../../types/appointments/ui"
 
 export const AppointmentsSection: React.FC = () => {
+    const [appointments, setAppointments] = React.useState<Appointment[]>([]);
+
     return (
         <div id="appointments-section">
             <h1>Appointments</h1>
+            <div id="appointments-list">
+                <ul>
+                    {appointments.map(appointment => (
+                        <li key={appointment.appointmentId} id={`appointment-${appointment.appointmentId}`}>
+                            <ul>
+                                <li>{appointment.appointmentType}</li>
+                                <li>User: {appointment.user?.givenName} {appointment.user?.familyName}</li>
+                                <li>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</li>
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
