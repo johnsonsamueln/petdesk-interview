34ac0d8febfc7d46e152177e3b2a508a30cb26c5
Sam <decoyoctorok@gmail.com>
2023-07-15 20:47:48 -0700

    Renamed First/Last name to Given/Family

--- src/Controllers/Appointments/Models/User.cs
+++ src/Controllers/Appointments/Models/User.cs
@@ -3,8 +3,8 @@ namespace petdesk_interview_app.Controllers.Appointments.Models
     public class User
     {
         public int UserId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? GivenName { get; set; }
+        public string? FamilyName { get; set; }
         public string? VetDataId { get; set; }
     }
 }
