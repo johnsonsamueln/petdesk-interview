aaa9b039a64eabd122dc692ebabd57f7ede41434
Sam <decoyoctorok@gmail.com>
2023-07-15 23:11:27 -0700

    Added borders around each AppointmentDetail

--- src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Appointment } from "../../../types/appointments/ui";
+import "./AppointmentDetail.css"
 
 type Props = {
     appointment: Appointment
 }
 export const AppointmentDetails: React.FC<Props> = ({ appointment }) => {
     return (
-        <ul>
+        <ul id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
             <li>{appointment.appointmentType}</li>
             <li>User: {appointment.user?.givenName} {appointment.user?.familyName}</li>
             <li>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</li>
