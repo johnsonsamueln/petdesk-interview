4b407053cd64aae616a81cd1f313957806bb4979
Sam <decoyoctorok@gmail.com>
2023-07-15 23:22:33 -0700

    Lay out User and Animal details side-by-side

--- src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
+++ src/ClientApp/src/components/sections/Appointments/AppointmentDetails.tsx
@@ -9,8 +9,10 @@ export const AppointmentDetails: React.FC<Props> = ({ appointment }) => {
     return (
         <div id={`appointment-detail-${appointment.appointmentId}`} className="appointment-detail">
             <h3>{appointment.appointmentType}</h3>
-            <div>User: {appointment.user?.givenName} {appointment.user?.familyName}</div>
-            <div>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</div>
+            <div className="appointment-detail-body">
+                <div>User: {appointment.user?.givenName} {appointment.user?.familyName}</div>
+                <div>Animal: {appointment.animal?.givenName} {`(${appointment.animal?.species}, ${appointment.animal?.breed})`}</div>
+            </div>
         </div>
     )
 }
