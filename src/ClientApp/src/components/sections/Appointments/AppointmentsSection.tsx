65975128c28f4d59585bf2458f7daaa1baac4467
Sam <decoyoctorok@gmail.com>
2023-07-15 21:30:10 -0700

    Started creating AppointmentsSection

---
+++ src/ClientApp/src/components/sections/Appointments/AppointmentsSection.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+export const AppointmentsSection: React.FC = () => {
+    return (
+        <div id="appointments-section">
+            <h1>Appointments</h1>
+        </div>
+    )
+}
