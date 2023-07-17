c66daeef7b64afdde692dab8493c0ee1250af432
Sam <decoyoctorok@gmail.com>
2023-07-17 00:14:46 -0700

    Ensure that spinner covers the modal

--- src/ClientApp/src/components/FixedSpinner.tsx
+++ src/ClientApp/src/components/FixedSpinner.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 export const FixedSpinner: React.FC = () => {
     return (
-        <div className="fixed-spinner-bg">
+        <div className="fixed-spinner-wrapper">
             <div className="spinner-border fixed-spinner" />
         </div>
     )
