50276129044e6f64d9fd5f5d61ba3e72ecd44377
Sam <decoyoctorok@gmail.com>
2023-07-16 22:59:22 -0700

    Use react-bootstrap package for modal

--- src/ClientApp/src/components/sections/Appointments/RescheduleModal.tsx
+++ src/ClientApp/src/components/sections/Appointments/RescheduleModal.tsx
@@ -1,23 +1,20 @@
 import * as React from "react"
+import { Modal } from "react-bootstrap"
 import { Appointment } from "../../../types/appointments/ui"
 
 type RescheduleModalProps = {
     appointment: Appointment;
     onCloseModal: () => void;
 }
-export const RescheduleModal : React.FC<RescheduleModalProps> = () => {
+export const RescheduleModal: React.FC<RescheduleModalProps> = () => {
     return (
-        <div className="modal">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">Reschedule Appointment</div>
-                    <div className="modal-body">Select a new appointment date:</div>
-                    <div className="modal-footer">
-                        <button>Cancel</button>
-                        <button>Request reschedule</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Modal show={true}>
+            <Modal.Header>Reschedule Appointment</Modal.Header>
+            <Modal.Body>Select a new appointment date:</Modal.Body>
+            <Modal.Footer>
+                <button className="btn btn-secondary">Cancel</button>
+                <button className="btn btn-primary">Request reschedule</button>
+            </Modal.Footer>
+        </Modal>
     )
 }
