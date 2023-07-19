55378d41e9667660c1c3cbea0cc7fe922e7e0050
johnsonsamueln <38303032+johnsonsamueln@users.noreply.github.com>
2023-07-18 22:20:52 -0700

    Added a sticky header with "Scroll to top" button (#15)

--- src/ClientApp/src/components/Dashboard.tsx
+++ src/ClientApp/src/components/Dashboard.tsx
@@ -3,9 +3,32 @@ import "./Dashboard.css"
 import { AppointmentsSection } from "./sections/Appointments/AppointmentsSection"
 
 export const Dashboard: React.FC = () => {
+    const [shouldShowScrollButton, setShouldShowScrollButton] = React.useState(false);
+
+    React.useEffect(() => {
+        const scrollListener = () => {
+            const isScrolled = window.scrollY > 300
+            setShouldShowScrollButton(isScrolled);
+        }
+        window.addEventListener("scroll", scrollListener);
+
+        return () => window.removeEventListener("scroll", scrollListener);
+    }, [])
+
+    const scrollToTop = () => window.scrollTo(0, 0);
+
     return (
         <div id="dashboard">
-            <h1 className="title">Dashboard</h1>
+            <header className="dashboard-header">
+                <div className="dashboard-header-left">
+                    <h1 className="dashboard-header-title">Dashboard</h1>
+                </div>
+                <div className="dashboard-header-right">
+                    {shouldShowScrollButton && (
+                        <button className="dashboard-header-top-button" onClick={scrollToTop}>Scroll to top</button>
+                    )}
+                </div>
+            </header>
             <AppointmentsSection />
         </div>
     )
