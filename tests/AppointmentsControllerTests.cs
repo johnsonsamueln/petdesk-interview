946e287c1a30ac8b9a29030d368460a38c7f669b
Sam <decoyoctorok@gmail.com>
2023-07-18 22:53:45 -0700

    Create & inject mocks into sut

--- tests/AppointmentsControllerTests.cs
+++ tests/AppointmentsControllerTests.cs
@@ -1,3 +1,9 @@
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Moq;
+using petdesk_interview_app.Controllers.Appointments;
+using petdesk_interview_app.External.Pstmn;
+
 namespace petdesk_interview_app_tests;
 
 [TestClass]
@@ -5,9 +11,17 @@ public class AppointmentsControllerTests
 {
     private readonly AppointmentsController sut;
 
+    private readonly Mock<ILogger<AppointmentsController>> mockLogger;
+    private readonly Mock<IPstmn> mockPstmn;
+    private readonly Mock<IMapper> mockMapper;
+
     public AppointmentsControllerTests()
     {
-        sut = new AppointmentsController();
+        mockLogger = new Mock<ILogger<AppointmentsController>>();
+        mockPstmn = new Mock<IPstmn>();
+        mockMapper = new Mock<IMapper>();
+
+        sut = new AppointmentsController(mockLogger.Object, mockPstmn.Object, mockMapper.Object);
     }
 
     [TestMethod]
