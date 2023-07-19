03bdd88fe455a34947445d2b89ae6d938961c687
Sam <decoyoctorok@gmail.com>
2023-07-18 22:46:31 -0700

    Started creating unit test class for controller

---
+++ tests/AppointmentsControllerTests.cs
@@ -0,0 +1,17 @@
+namespace petdesk_interview_app_tests;
+
+[TestClass]
+public class AppointmentsControllerTests
+{
+    private readonly AppointmentsController sut;
+
+    public AppointmentsControllerTests()
+    {
+        sut = new AppointmentsController();
+    }
+
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
