fa15000434ca4701789f7dc383c7ff095616c1e8
Sam <decoyoctorok@gmail.com>
2023-07-18 22:08:35 -0700

    Added left and right sections to header

--- src/ClientApp/src/components/Dashboard.tsx
+++ src/ClientApp/src/components/Dashboard.tsx
@@ -6,7 +6,12 @@ export const Dashboard: React.FC = () => {
     return (
         <div id="dashboard">
             <header className="dashboard-header">
-                <h1>Dashboard</h1>
+                <div className="dashboard-header-left">
+                    <h1 className="dashboard-header-title">Dashboard</h1>
+                </div>
+                <div className="dashboard-header-right">
+                    <button className="dashboard-header-top-button">Scroll to top</button>
+                </div>
             </header>
             <AppointmentsSection />
         </div>
